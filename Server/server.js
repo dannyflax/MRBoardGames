79a78115e40dad0f599095d000002247589d3016
ViralPanda <patel.2004@osu.edu>
2016-11-20 08:46:53 -0500

    added holding cells for objects being held.

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,5 @@
 //setup
-var PORT = 3901;
+var PORT = 3901, holding = 'holding', objectsHeld = 'objectsHeld';
 
 var express = require('express'),
 UUID = require('node-uuid'),
@@ -13,7 +13,8 @@ var players = {}; //keeps track of players.
 //gameID (used as keys in games)
 //playerID (used as key players and values in games)
 //mutexLock
-//gamestate
+//gamestate - passed by client
+//objectsHeld - objects currently held by all clients in game. array of 2-tuple where key is object id and value is player holding.
 
 
 //gamestate
@@ -54,7 +55,7 @@ io.on('connection', function(client){
 		
 		playersLock.writeLock(function(){
 			var playerId = "player" + Object.keys(players).length;
-			while((playerId in players)){
+			while((players.hasOwnProperty(playerId))){
 					playerId = "player" + Object.keys(players).length + Math.floor((Math.random() * 10) + 1);
 			}
 			
@@ -62,10 +63,16 @@ io.on('connection', function(client){
 			players[playerId] = socketId;
 			playersLock.unlock();
 			gamesLock.writeLock(function(){
-				var game = {state: data.state, players: [playerId], lock: locks.createMutex()};
+				var game = {state: data.state, players: [playerId], lock: locks.createMutex(), objectsHeld: {}};
+				if(data.hasOwnProperty(holding)){
+					var tuple = {};
+					tuple[data[holding]] = playerId;
+					game[objectsHeld] = tuple;
+				}
+				
 				var id = "game" + Object.keys(games).length;
 				
-				while(id in games){
+				while(games.hasOwnProperty(id)){
 					id = "game" + Object.keys(games).length + Math.floor((Math.random() * 10) + 1);
 				}
 			
@@ -95,7 +102,7 @@ io.on('connection', function(client){
 				var game = games[data.gameID];
 				game.lock.lock(function(){
 					game.players.push(playerId);
-					client.emit('gameJoined', {success: true, playerID: playerId});
+					client.emit('gameJoined', {success: true, playerID: playerId, state: games[data.gameID].state, objectsHeld: games[data.gameID].objectsHeld});
 					game.lock.unlock();
 				});
 			});
@@ -129,13 +136,40 @@ io.on('connection', function(client){
 	});
 	
 	
+	//holding:
+	//objectsHeld
+	
 	client.on('updateGameState', function(data){
 		console.log("UPDATE GAME STATE\n" + JSON.stringify(data));
 		
 		if(games[data.gameID] && players[data.playerID] && games[data.gameID].players.indexOf(data.playerID) >= 0){
 			var game = games[data.gameID];
+			
+			if(data.hasOwnProperty(holding) &&  game.objectsHeld.hasOwnProperty(data.holding) && game.objectsHeld[data.holding] != data.playerID){
+				client.emit('gameStateUpdated', {success: false, gameState: "Illegal game state update"});
+				return;
+			}
+			
+			
 			game.lock.lock(function(){
 				game.state = data.state;
+				
+				var delEntry = false;
+				if(!(data.hasOwnProperty(holding))){
+					for(var key in game.objectsHeld){
+						if(game.objectsHeld[key] == data.playerID){
+							delEntry = key;
+							break;
+						}
+					}
+					if(delEntry){
+						delete game.objectsHoldingp[delEntry];
+					}
+				}else{
+					console.log("Execing else part to set objectsHeld");
+					game.objectsHeld[data[holding]] = data.playerID;
+				}
+					
 				game.lock.unlock();
 				var playersInGame = game.players;
 				console.log(playersInGame);
@@ -143,16 +177,16 @@ io.on('connection', function(client){
 					var player = playersInGame[i];
 					console.log(JSON.stringify(players) + "\n" + player);
 					//console.log(io.sockets.connected[players[player]]);
-					io.sockets.connected[players[player]].emit('gameStateUpdated', {player: data.playerID, state: data.state});
+					io.sockets.connected[players[player]].emit('gameStateUpdated', {success: true, player: data.playerID, state: data.state, objectsHeld: game.objectsHeld});
 				}
 			});
 		}else{
-			client.emit('gameStateUpdated', {gameState: "Fuck you for trying to update state of game that you are not in"});
+			client.emit('gameStateUpdated', {success: false, gameState: "Fuck you for trying to update state of game that you are not in"});
 		}
 	});
 	
 	client.on('getGameState', function(data){
 		console.log("GET GAME STATE \n" + JSON.stringify(data));
-		client.emit('gameState', {state: games[data.gameID].state});
+		client.emit('gameState', {state: games[data.gameID].state, objectsHeld: game.objectsHeld});
 	});
 });
